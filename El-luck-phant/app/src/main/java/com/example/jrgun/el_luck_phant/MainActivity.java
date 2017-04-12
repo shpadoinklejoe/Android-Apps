9670d85dfada4db06e4b98a21118be9a5b98d08c
jrgunderson <jrgunderson@cpp.edu>
2017-04-11 18:02:13 -0700

    commit

--- El-luck-phant/app/src/main/java/com/example/jrgun/el_luck_phant/MainActivity.java
+++ El-luck-phant/app/src/main/java/com/example/jrgun/el_luck_phant/MainActivity.java
@@ -2,15 +2,13 @@ package com.example.jrgun.el_luck_phant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pick1);
 
     }
 }
