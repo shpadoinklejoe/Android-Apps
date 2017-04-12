1624c0cd8b56a4ca10b3005f0bd60bdd7bcaa7d1
jrgunderson <jrgunderson@cpp.edu>
2017-04-12 14:59:09 -0700

    commit

--- El-luck-phant/app/src/main/java/com/example/jrgun/el_luck_phant/MainActivity.java
+++ El-luck-phant/app/src/main/java/com/example/jrgun/el_luck_phant/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.jrgun.el_luck_phant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick1);
+        this.setContentView(R.layout.activity_start);
+
+        // Touch anywhere to continue
+        LinearLayout startScreen = (LinearLayout)findViewById(R.id.startScreen);
+        startScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity( new Intent(MainActivity.this, MainMenuActivity.class) );
+            }
+        });
+
+
 
     }
 }
---
+++ El-luck-phant/app/src/main/java/com/example/jrgun/el_luck_phant/MainMenuActivity.java
@@ -0,0 +1,43 @@
+package com.example.jrgun.el_luck_phant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by jrgun on 4/12/2017.
+ */
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_mainmenu);
+
+
+        // to 1 Play
+        Button to1PlayFromMain = (Button)findViewById(R.id.to1PlayFromMain);
+        to1PlayFromMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity( new Intent(MainMenuActivity.this, OnePlayActivity.class) );
+            }
+        });
+
+
+        // to 5 Plays
+        Button to5PlaysFromMain = (Button)findViewById(R.id.to5PlaysFromMain);
+        to5PlaysFromMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity( new Intent(MainMenuActivity.this, FivePlayActivity.class) );
+            }
+        });
+
+    }
+}
