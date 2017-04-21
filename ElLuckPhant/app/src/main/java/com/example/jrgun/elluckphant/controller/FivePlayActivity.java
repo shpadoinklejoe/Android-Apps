1a683514631bb18686437e890d76cab704ba26f6
jrgunderson <jrgunderson@cpp.edu>
2017-04-21 15:34:42 -0700

    commit

--- ElLuckPhant/app/src/main/java/com/example/jrgun/elluckphant/controller/FivePlayActivity.java
+++ ElLuckPhant/app/src/main/java/com/example/jrgun/elluckphant/controller/FivePlayActivity.java
@@ -8,14 +8,20 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.jrgun.elluckphant.R;
+import com.example.jrgun.elluckphant.model.GenerateNumbers;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by jrgun on 4/11/2017.
  */
 
-public class FivePlayActivity extends AppCompatActivity {
+public class FivePlayActivity extends AppCompatActivity
+{
 
     private ArrayList<ArrayList<TextView>> balls;
 
@@ -24,6 +30,9 @@ public class FivePlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_5plays);
+
+        // fill TextView Arrays
+        fillBalls();
 
         // to main
         Button toMainFrom5Plays = (Button)findViewById(R.id.toMainFrom5Plays);
@@ -52,6 +61,18 @@ public class FivePlayActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                for(int i=0; i<5; ++i)
+                {
+                    // generate lotto numbers
+                    ArrayList<Integer> lottoNumbers = new GenerateNumbers().getNums();
+                    System.out.println(lottoNumbers);
+
+                    // update ball TextViews with lotto numbers
+                    for(int j=0; j<6; ++j)
+                    {
+                        balls.get(i).get(j).setText( Integer.toString(lottoNumbers.get(j)) );
+                    }
+                }
 
             }
 
@@ -60,6 +81,46 @@ public class FivePlayActivity extends AppCompatActivity {
     } // end onCreate
 
 
+    // add ball TextViews to an array
+    void fillBalls()
+    {
+        balls = new ArrayList<ArrayList<TextView>>();
+        balls.add(new ArrayList<TextView>(Arrays.asList(
+            (TextView) findViewById(R.id.a1),
+            (TextView) findViewById(R.id.a2),
+            (TextView) findViewById(R.id.a3),
+            (TextView) findViewById(R.id.a4),
+            (TextView) findViewById(R.id.a5),
+            (TextView) findViewById(R.id.a6) )));
+        balls.add(new ArrayList<TextView>(Arrays.asList(
+            (TextView) findViewById(R.id.b1),
+            (TextView) findViewById(R.id.b2),
+            (TextView) findViewById(R.id.b3),
+            (TextView) findViewById(R.id.b4),
+            (TextView) findViewById(R.id.b5),
+            (TextView) findViewById(R.id.b6) )));
+        balls.add(new ArrayList<TextView>(Arrays.asList(
+            (TextView) findViewById(R.id.c1),
+            (TextView) findViewById(R.id.c2),
+            (TextView) findViewById(R.id.c3),
+            (TextView) findViewById(R.id.c4),
+            (TextView) findViewById(R.id.c5),
+            (TextView) findViewById(R.id.c6) )));
+        balls.add(new ArrayList<TextView>(Arrays.asList(
+            (TextView) findViewById(R.id.d1),
+            (TextView) findViewById(R.id.d2),
+            (TextView) findViewById(R.id.d3),
+            (TextView) findViewById(R.id.d4),
+            (TextView) findViewById(R.id.d5),
+            (TextView) findViewById(R.id.d6) )));
+        balls.add(new ArrayList<TextView>(Arrays.asList(
+            (TextView) findViewById(R.id.e1),
+            (TextView) findViewById(R.id.e2),
+            (TextView) findViewById(R.id.e3),
+            (TextView) findViewById(R.id.e4),
+            (TextView) findViewById(R.id.e5),
+            (TextView) findViewById(R.id.e6) )));
+    }
 
 
 } // end class
